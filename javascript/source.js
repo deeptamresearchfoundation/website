fd8fa924c1c31f98e136821f3a60b2f1d0eae5a6
deeptamresearchfoundation <80377220+deeptamresearchfoundation@users.noreply.github.com>
2021-03-10 10:25:50 +0530

    Deeptam Research Foundation Website in github

---
+++ javascript/source.js
@@ -0,0 +1,19 @@
+var a;
+
+function show_hide() {
+    if (a == 0) {
+        document.getElementById("mobileshow").style.display = "none";
+        return a = 1;
+    }
+
+    else {
+        document.getElementById("mobileshow").style.display = "block";
+        return a = 0;
+    }
+}
+
+function hide() {
+    document.getElementById("mobileshow").style.display = "none";
+    return a = 1;
+}
+
