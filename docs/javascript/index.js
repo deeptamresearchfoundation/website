642165876b5e5e0d5ef54a92c586a0f80b930306
deeptamresearchfoundation <80377220+deeptamresearchfoundation@users.noreply.github.com>
2021-03-10 18:47:50 +0530

    Add files via upload

---
+++ docs/javascript/index.js
@@ -0,0 +1,35 @@
+function sendemail() {
+
+    var name = $('#Name').val();
+    var email = $('#Email').val();
+    var subject = $('#message').val();
+    var message = $('#message').val();
+
+    var Body = 'Name: ' + name + '<br>Email: ' + email + '<br>message: ' + subject + '<br>Message: ' + message;
+
+    Email.send({
+        // secure-token: c67899b3-aabc-440b-8751-860e9f33bb1b;
+
+        Host : "smtp.gmail.com",
+        Username : "deeptamresearchfoundationweb@gmail.com",
+        Password : "aayushkumarjha@drf",
+        To : 'deeptamresearchfoundation@gmail.com',
+        From : "deeptamresearchfoundation@gmail.com",
+        Subject : "New message on contact us form " + name,
+        Body : Body
+
+    }).then(
+        message => {
+            //console.log (message);
+            if (message == 'OK') {
+                window.location.replace("../Contactus/thankyou/index.html")
+            }
+            else {
+                console.error(message);
+                window.location.replace("../Contactus/error/index.html")
+
+            }
+
+        }
+    );
+}
