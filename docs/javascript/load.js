e53149b82eead756d064585e4f7a33586165733a
deeptamresearchfoundation <80377220+deeptamresearchfoundation@users.noreply.github.com>
2021-04-17 16:31:11 +0530

    Update load.js

--- docs/javascript/load.js
+++ docs/javascript/load.js
@@ -1,4 +1,6 @@
 function onloadispreload() {
     document.getElementById('preloader').style.display='none';
     document.getElementById('bodycontent').style.display='block';
+    a = $(".navbar");
+    a.addClass('sticky-top');
 }
