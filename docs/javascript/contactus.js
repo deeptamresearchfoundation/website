24a0d77d5ebc2090e0f27388f1d5da2ca3d5bacd
deeptamresearchfoundation <80377220+deeptamresearchfoundation@users.noreply.github.com>
2021-04-18 13:57:28 +0530

    Add files via upload

---
+++ docs/javascript/contactus.js
@@ -0,0 +1,30 @@
+function sendemail() {
+
+    var name = $('#Name').val();
+    var email = $('#Email').val();
+    var message = $('#message').val();
+
+    var Body = 'Name: ' + name + '<br>Email: ' + email + '<br>Message: ' + message;
+
+    Email.send({
+        SecureToken: "31c101ed-19a9-4ff2-9d94-ba71df93dba2",
+        To: 'deeptamresearchfoundation@gmail.com',
+        From: "deeptamresearchfoundation@gmail.com",
+        Subject: "New message on contact us form " + name,
+        Body: Body
+
+    }).then(
+        message => {
+            //console.log (message);
+            if (message == 'OK') {
+                window.location.replace("../Contact Us/thankyou.html")
+            }
+            else {
+                console.error(message);
+                window.location.replace("../Contact Us/error.html")
+
+            }
+
+        }
+    );
+}
