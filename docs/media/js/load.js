9d8d9d958f0aea6dd1dee6d255dce46b10c84191
deeptamresearchfoundation <80377220+deeptamresearchfoundation@users.noreply.github.com>
2021-04-25 21:45:35 +0530

    Delete docs directory

--- docs/media/js/load.js
+++
@@ -1,6 +0,0 @@
-function onloadispreload() {
-    document.getElementById('preloader').style.display = 'none';
-    document.getElementById('bodycontent').style.display = 'block';
-    a = $(".navbar");
-    a.addClass('sticky-top');
-}
